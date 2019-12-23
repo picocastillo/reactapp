2cc474ad08a4b4c8cf960d268f9f760a8108ccc9
picocastillo <you@example.com>
2019-12-23 16:52:44 -0300

    Working react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './pages/Home';
+import All from './pages/All';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <Switch>
+         <Route exact path="/home">
+           <Home />
+         </Route>
+         <Route exact path="/all">
+           <All/>
+         </Route>
+       </Switch>
+   </Router>
+
   );
 }
 
---
+++ src/pages/All.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+class All extends React.Component {
+  state = {
+    form: {
+      email: '',
+      text: ''
+    }
+  }
+  handleChange = e => {
+    this.setState({
+      form: {
+        ... this.state.form,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+  render(){
+    return(
+      <div>
+        <Header/>
+        <p>WE ARE ALL
+        </p>
+      </div>
+    )
+  }
+}
+
+export default All;
