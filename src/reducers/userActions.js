3f3ec5a8c650a2a38b9d621fefbb936848b4ba18
Pico <castillo.cesar.pico@gmail.com>
2019-12-26 13:11:13 -0300

    Addet dype action file

--- src/reducers/userActions.js
+++ src/reducers/userActions.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-
+import {GET_ALL} from '../types/userTypes';
 
 
 export const getAll = () => async (dispatch) => {
   const res = await axios.get('https://jsonplaceholder.typicode.com/users');
 
   dispatch({
-    type: 'get_users',
+    type: GET_ALL,
     payload: res.data
   })
 }
